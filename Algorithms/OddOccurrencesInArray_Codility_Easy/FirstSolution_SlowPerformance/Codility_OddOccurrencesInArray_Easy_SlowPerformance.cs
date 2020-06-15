5be088eaab8f5f22a42e64dd8ea294ff55b23e0d
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-06-15 21:10:48 +0200

    Add new Algorithm

---
+++ Algorithms/OddOccurrencesInArray_Codility_Easy/FirstSolution_SlowPerformance/Codility_OddOccurrencesInArray_Easy_SlowPerformance.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Algorithms.OddOccurrencesInArray_Codility_Easy.FirstSolution_SlowPerformance
+{
+    /// <summary>
+    /// https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+    /// </summary>
+    class Codility_OddOccurrencesInArray_Easy_SlowPerformance
+    {
+        /// <summary>
+        /// n*n time complexity
+        /// </summary>
+        /// <param name="a">non empty number array</param>
+        /// <returns>Odd occurent number</returns>
+        public static int Solution(int[] a)
+        {
+            int x = a[0];
+            for (int i = 0; i < a.Length; i++)
+            {
+                x = a[i];
+                int c = 0;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (x == a[j])
+                    {
+                        c++;
+                    }
+                }
+                if (c % 2 == 1)
+                {
+                    return x;
+                }
+            }
+
+            return x;
+        }
+    }
+}
---
+++ Algorithms/OddOccurrencesInArray_Codility_Easy/SecondSolution_BetterPerformance/Codility_OddOccurrencesInArray_Easy_BetterPerformance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.OddOccurrencesInArray_Codility_Easy.SecondSolution_BetterPerformance
+{
+    /// <summary>
+    /// https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
+    /// </summary>
+    class Codility_OddOccurrencesInArray_Easy_BetterPerformance
+    {
+        /// <summary>
+        /// O(N) or O(N*log(N)) time complexity
+        /// </summary>
+        /// <param name="a">non empty number array</param>
+        /// <returns>The odd occurent number</returns>
+        public int Solution(int[] a)
+        {
+            Array.Sort(a);
+            int c = 0;
+            int temp = a[0];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != temp && c % 2 == 1)
+                {
+                    break;
+                }
+
+                temp = a[i];
+                c++;
+
+                if (a[i] != temp)
+                {
+                    return temp;
+                }
+            }
+
+            return temp;
+        }
+    }
+}
