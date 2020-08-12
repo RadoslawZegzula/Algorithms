40d5f87aa70b369b8e718d2fb211fac8231da6fe
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-12 22:29:53 +0200

    Add example test

---
+++ AlgorithmsXUnitTests/BinaryGap_Codility_Easy_Tests/BinaryGap_Codility_Easy_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.BinaryGap_Codility_Easy;
+using Xunit;
+
+namespace AlgorithmsXUnitTests.BinaryGap_Codility_Easy_Tests
+{
+    public class BinaryGap_Codility_Easy_Tests
+    {
+        [Theory]
+        [InlineData(1041, 5)]
+        [InlineData(15, 0)]
+        [InlineData(32, 0)]
+        public void ExampleTests(int n, int expected)
+        {
+            int result = BinaryGap_Codility_Easy.FindLongestSequenceOfBinaryZeros(n);
+
+            Assert.Equal(expected, result);
+        }
+
+    }
+}
