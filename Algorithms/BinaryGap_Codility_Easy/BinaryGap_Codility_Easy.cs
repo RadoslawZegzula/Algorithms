adca4b906ddc345d27eecc21a526aab62e5cf8df
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-12 22:16:10 +0200

    Make class and method static

--- Algorithms/BinaryGap_Codility_Easy/BinaryGap_Codility_Easy.cs
+++ Algorithms/BinaryGap_Codility_Easy/BinaryGap_Codility_Easy.cs
@@ -5,9 +5,9 @@ namespace Algorithms.BinaryGap_Codility_Easy
     /// <summary>
     /// https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
     /// </summary>
-    class BinaryGap_Codility_Easy
+    public static class BinaryGap_Codility_Easy
     {
-        public int FindLongestSequenceOfBinaryZeros(int n)
+        public static int FindLongestSequenceOfBinaryZeros(int n)
         {
             // write your code in C# 6.0 with .NET 4.5 (Mono)
             var binary = Convert.ToString(n, 2);
