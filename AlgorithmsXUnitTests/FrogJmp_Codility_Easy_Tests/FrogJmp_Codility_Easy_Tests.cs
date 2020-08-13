c6d6601b3871d70d5993b1ace4c6240a0bc92142
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-13 20:22:22 +0200

    Add test class

---
+++ AlgorithmsXUnitTests/FrogJmp_Codility_Easy_Tests/FrogJmp_Codility_Easy_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.FrogJmp_Codility_Easy;
+using Xunit;
+
+namespace AlgorithmsXUnitTests.FrogJmp_Codility_Easy_Tests
+{
+    public class FrogJmp_Codility_Easy_Tests
+    {
+        [Theory]
+        [InlineData(10, 85, 30, 3)]
+        public void ExampleTests(int x, int y, int d, int expected)
+        {
+            int result = FrogJmp_Codility_Easy.CountMinimalNumberOfJumpsFromXToY(x, y, d);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
