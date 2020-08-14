44c6956670682ee70982d2cfc72ae01a1083d56d
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-14 20:10:20 +0200

    Add test classes

---
+++ AlgorithmsXUnitTests/OddOccurrencesInArray_Codility_Easy_Tests/FirstSolution_SlowPerformance_Tests/Codility_OddOccurrencesInArray_Easy_SlowPerformance_Tests.cs
@@ -0,0 +1,20 @@
+﻿using Algorithms.OddOccurrencesInArray_Codility_Easy.FirstSolution_SlowPerformance;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AlgorithmsXUnitTests.OddOccurrencesInArray_Codility_Easy_Tests.FirstSolution_SlowPerformance_Tests
+{
+    public class Codility_OddOccurrencesInArray_Easy_SlowPerformance_Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 9, 3, 9, 3, 9, 7, 9}, 7)]
+        public void ExampleTest(int[] arr, int expected)
+        {
+            int result = Codility_OddOccurrencesInArray_Easy_SlowPerformance.Solution(arr);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AlgorithmsXUnitTests/OddOccurrencesInArray_Codility_Easy_Tests/SecondSolution_BetterPerformance_Tests/Codility_OddOccurrencesInArray_Easy_BetterPerformance_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.OddOccurrencesInArray_Codility_Easy.SecondSolution_BetterPerformance;
+using Xunit;
+
+namespace AlgorithmsXUnitTests.OddOccurrencesInArray_Codility_Easy_Tests.SecondSolution_BetterPerformance_Tests
+{
+    public class Codility_OddOccurrencesInArray_Easy_BetterPerformance_Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 9, 3, 9, 3, 9, 7, 9 }, 7)]
+        public void ExampleTest(int[] arr, int expected)
+        {
+            int result = Codility_OddOccurrencesInArray_Easy_BetterPerformance.Solution(arr);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
