9b499b0b1cc9fb03eaffe2206dd79be5ac4db379
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-18 14:59:59 +0200

    Remove unnecessary comment

--- Algorithms/CyclicRotation_Codility_Easy/CyclicRotation_Codility_Easy.cs
+++ Algorithms/CyclicRotation_Codility_Easy/CyclicRotation_Codility_Easy.cs
@@ -11,7 +11,6 @@ namespace Algorithms.CyclicRotation_Codility_Easy
     {
         public int[] MoveNumbersRight(int[] a, int k)
         {
-            // write your code in C# 6.0 with .NET 4.5 (Mono)
             if (a.Length == 0 || k == 0)
             {
                 return a;
