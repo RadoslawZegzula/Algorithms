83831d33fab807816a707098600eb22d6a9cf3b1
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-08-19 13:16:56 +0200

    Add example test

---
+++ AlgorithmsXUnitTests/PermMissingElem_Codility_Easy_Tests/PermMissingElem_Codility_Easy_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Algorithms.PermMissingElem_Codility_Easy;
+using Xunit;
+
+namespace AlgorithmsXUnitTests.PermMissingElem_Codility_Easy_Tests
+{
+    public class PermMissingElem_Codility_Easy_Tests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 1, 5 }, 4)]
+        public void ExampleTests(int[] x, int expected)
+        {
+            int result = PermMissingElem_Codility_Easy.ReturnMissingElement(x);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
