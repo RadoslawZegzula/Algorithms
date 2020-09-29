8fe5670c3157c48631867f447da762629c6fc056
RadoslawZegzula <radoslaw.lukasz.zegzula@gmail.com>
2020-09-29 09:58:02 +0200

    Rename argument

--- AlgorithmsXUnitTests/PermMissingElem_Codility_Easy_Tests/PermMissingElem_Codility_Easy_Tests.cs
+++ AlgorithmsXUnitTests/PermMissingElem_Codility_Easy_Tests/PermMissingElem_Codility_Easy_Tests.cs
@@ -17,9 +17,9 @@ namespace AlgorithmsXUnitTests.PermMissingElem_Codility_Easy_Tests
         [Theory]
         [InlineData(new int[] { 1, 2, 3, 4, 5 }, 6)]
         [InlineData(new int[] { 2, 3, 4, 5, 6 }, 1)]
-        public void MissingFirstOrLast(int[] x, int expected)
+        public void MissingFirstOrLast(int[] array, int expected)
         {
-            int result = PermMissingElem_Codility_Easy.ReturnMissingElement(x);
+            int result = PermMissingElem_Codility_Easy.ReturnMissingElement(array);
 
             Assert.Equal(expected, result);
         }
